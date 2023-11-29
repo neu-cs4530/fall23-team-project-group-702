c3c556a77419319065f68bdfc935a586108e156c
capture120 <capture12020@gmail.com>
2023-11-29 08:41:29 -0500

    fixing frontend package.json scripts, fixed bad merge

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,13 +8,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    /* REQUIRED IN ORDER TO USE @Spotify-web-api-ts-sdk IN PRODUCTION */
-typescript: {
-    ignoreBuildErrors: true,
-    },
-}
-
-module.exports = nextConfig
